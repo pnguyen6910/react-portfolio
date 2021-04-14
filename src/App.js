e49644fa7d02ff7693bc824aa7eae48b7479af28
Paul <72827473+pnguyen6910@users.noreply.github.com>
2021-04-14 02:01:05 -0500

    revamping pages

--- src/App.js
+++ src/App.js
@@ -8,20 +8,30 @@ import PortfolioPage from './pages/portfolio'
 import HomePage from './pages/home'
 import AboutPage from './pages/about'
 import Nav from './components/Nav/nav'
+import "./App.css"
+import "./CYOA.png"
+import "./comingsoon.png"
+import "./battlestation.png"
+import "./moonbackground.png"
+import "./paul2.jpg"
+import "./paul6.png"
+import "./paulandmom.png"
+import "./ragingmongooses.png"
+import "./timedQuiz.png"
 
 function App() {
   return (
     <Router>
       <Nav />
       <Switch>
+        <Route path="/about">
+          <AboutPage />
+        </Route>
         <Route path="/contact">
           <ContactPage />
         </Route>
         <Route path="/portfolio">
           <PortfolioPage />
-        </Route>
-        <Route path="/about">
-          <AboutPage />
         </Route>
         <Route path="/">
           <HomePage />
---
+++ src/pages/navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar(props) {
+    return (
+        <nav class="navbar navbar-expand navbar-dark bg-dark">
+            <a class="navbar-brand name" href="#">Ryan Burnett</a>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav navItems">
+                    <li class="nav-item">
+                        <Link to="/" className="nav-link" href="#">Home</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/portfolio" className="nav-link" href="#">Portfolio</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/contact" className="nav-link" href="#">Contact</Link>
+                    </li>
+                    <li class="nav-item">
+                        <Link to="/about" className="nav-link" href="#">About</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
